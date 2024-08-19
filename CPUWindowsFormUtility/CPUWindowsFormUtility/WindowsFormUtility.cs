08fce6f134b7ec84a6d09cd20a0994ea47e556ad
moshefidds <moshefiddle1234@gmail.com>
2024-08-19 06:46:30 -0400

    15 completetd

--- CPUWindowsFormUtility/CPUWindowsFormUtility/WindowsFormUtility.cs
+++ CPUWindowsFormUtility/CPUWindowsFormUtility/WindowsFormUtility.cs
@@ -5,13 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace CPUWindowsFormUtility
-//{
-//    internal class WindowsFormUtility
-//    {
-//    }
-//}
-
 
 namespace CPUWindowsFormFramework
 {
